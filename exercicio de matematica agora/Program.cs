fe82b99c5085ceb8b0d203462172f20f2d284276
sibele <sibele.ramos@yahoo.com.brgit config --global user.name sibelegit log -pgit config --global user.email sibele.ramos@yahoo.com.br>
2021-07-31 19:10:18 -0300

    Baixando primeiro arquivo sozinha

---
+++ exercicio de matematica agora/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace exercicio_de_matematica_agora
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+
+        {
+            Console.WriteLine("Digite a nota do aluno");
+            string aluno = Console.ReadLine();
+            Console.WriteLine("Digite a nota 1 :");
+            double nota1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digite a nota 2 :");
+            double nota2 = Convert.ToDouble(Console.ReadLine());
+            double mediaAluno = (nota1 + nota2) / 2;
+
+            if (mediaAluno == 0)
+            {
+                Console.WriteLine($"Voce nao aprendeu nada");
+            }
+            else if (media>= $$ media <= 4);
+            {
+                Console.WriteLine("Voce esta reprovado");
+            }            
+            else if(media>= 5 && mediaAluno <= 7)
+            {
+                                         
+                         
+                  
+                                    
+            {
+                Console.WriteLine("Aluno" + aluno + "Foi Reprovado");
+                    Console.ReadKey();
+
+                    int n = 0;
+                    while (n < 5)
+                    {
+                        Console.Write(n);
+                        n++;
+                    }
+                    // Output:
+                    // 01234
+
+
+                }
+
+            }
+          
+          
+    }
+     
+
+    }
